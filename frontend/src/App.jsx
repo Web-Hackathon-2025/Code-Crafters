257c6ef1e45ac8ac2c73c60840a43b160e427ee4
Waleeja Ali <waleeja.ali25@gmail.com>
2025-12-19 11:11:45 +0500

    fix app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,27 @@
-import { Routes, Route, Link } from "react-router-dom";
-import './App.css'
-import Login from "./users/Login"
-import Signup from "./users/Signup"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import Auth from "./service_provider/Auth"
-import ServiceProviderDashboard from "./service_provider/ServiceProviderDashboard"
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+import { Routes, Route } from "react-router-dom";
+import './App.css';
+import Login from "./users/Login";
+import Signup from "./users/Signup";
+import Auth from "./service_provider/Auth";
+import ServiceProviderDashboard from "./service_provider/ServiceProviderDashboard";
 import AdminSignIn from './admin/AdminLogin';
 import Dashboard from './admin/Dashboard';
 
-
->>>>>>> Stashed changes
 function App() {
-
-
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="signup" element={<Signup/>}/>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-      <Route path="service-provider/auth" element={<Auth/>}/>
-      <Route path="service-provider/dashboard" element={<ServiceProviderDashboard/>}/>
-    
-=======
-      <Route path="/admin" element={<AdminSignIn/>}/>
-      <Route path="/admin/dashboard" element={<Dashboard/>}/>
->>>>>>> Stashed changes
-=======
-      <Route path="/admin" element={<AdminSignIn/>}/>
-      <Route path="/admin/dashboard" element={<Dashboard/>}/>
->>>>>>> Stashed changes
-=======
-      <Route path="/admin" element={<AdminSignIn/>}/>
-      <Route path="/admin/dashboard" element={<Dashboard/>}/>
->>>>>>> Stashed changes
+      <Route path="signup" element={<Signup />} />
+
+      {/* Service Provider Routes */}
+      <Route path="service-provider/auth" element={<Auth />} />
+      <Route path="service-provider/dashboard" element={<ServiceProviderDashboard />} />
+
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminSignIn />} />
+      <Route path="/admin/dashboard" element={<Dashboard />} />
     </Routes>
-  )
+  );
 }
 
-export default App
-
+export default App;
