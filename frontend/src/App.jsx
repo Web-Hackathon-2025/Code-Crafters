a14dec6e9af59de8972c90b2ff3bf1b5535c593d
Khadija-Saeed08 <saeedkhadija782@gmail.com>
2025-12-19 11:49:15 +0500

    routing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,40 @@
 import { useState } from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
-import Login from "./users/Login"
-import Signup from "./users/Signup"
+
+
 import Auth from "./service_provider/Auth"
 import ServiceProviderDashboard from "./service_provider/ServiceProviderDashboard"
 import ServiceManagement from "./service_provider/ServiceManagement"
 import AdminSignIn from './admin/AdminLogin';
 import Dashboard from './admin/Dashboard';
+import CustomerAuth from "./users/CustomerAuth"
+import ServiceRequestsHistory from './service_provider/ServiceRequestHistory';
 
-import AdminSignIn from './admin/AdminLogin';
-import Dashboard from './admin/Dashboard';
 
 
-import ServiceManagement from "./service_provider/ServiceManagement"
+
 function App() {
+
+
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="signup" element={<Signup/>}/>
+      <Route path="/" element={<CustomerAuth />} />
+
       <Route path="service-provider/auth" element={<Auth/>}/>
       <Route path="service-provider/dashboard" element={<ServiceProviderDashboard/>}/>
       <Route path="service-provider/service-management" element={<ServiceManagement/>}/>
+      <Route path="service-provider/service-request" element={<ServiceRequestsHistory/>}/>
     
       <Route path="/admin" element={<AdminSignIn/>}/>
       <Route path="/admin/dashboard" element={<Dashboard/>}/>
 
-      <Route path="/admin" element={<AdminSignIn/>}/>
-      <Route path="/admin/dashboard" element={<Dashboard/>}/>
 
-      <Route path="/admin" element={<AdminSignIn/>}/>
-      <Route path="/admin/dashboard" element={<Dashboard/>}/>
+
+
+
+
 
     </Routes>
-  );
+  )
 }
-
-export default App;
